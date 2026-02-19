e360a3b66dca2fc8e93888551c950022c5c97c40
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-02-19 19:09:27 +0200

    feat: clear

--- objects/immu-obs.js
+++ objects/immu-obs.js
@@ -1,12 +1,22 @@
 const obj = { id: 1, firstname: "Anna" }
 
 // Mutable Add
-obj.lastname = "Smith"; // add new property
+obj.lastname = "Smith";  // add new property
 
-// Immutable Add
-// Without [] syntax, the field name will be "field" instead of the value of the field variable
-const addToobj = (obj, field, value) => ({...obj, [field]: value}) // retunrn new object with added property
-
+// Immutable add
+// without [] syntax, the field name will be "field" instead of the value of 
+//the field variable 
+const addToobj = (obj, field, value) => ({ ...obj, [field]: value }) 
 const obj2 = addToobj(obj, "lastname", "Smith");
+console.log(obj2)  // { id: 1, firstname: 'Anna', lastname: 'Smith' }
 
-console.log(obj2);
+// Immutable update
+const updateObj = (obj, field, value) => ({ ...obj, [field]: value })
+const obj3 = updateObj(obj2, "firstname", "Maria");
+console.log(obj3)  // { id: 1, firstname: 'Maria', lastname: 'Smith' }
+
+// Immutable delete
+const deleteFromObj = (obj, field) => {
+    const { [field]: _, ...objToReturn } = obj;  // destructuring assignment to exclude the field
+    return objToReturn;
+}
--- objects/json2.js
+++ objects/json2.js
@@ -1,10 +1,10 @@
-const jsonStr = `{
+const jsonStr = ` {
     "id": 1,
     "firstname": "John",
     "lastname": "Smith",
     "age": 30,
     "isTeacher": false,
-    "hobbies": {"football", "music"},
+    "hobbies": ["football", "music"],
     "address": {
         "city": "Athens",
         "country": "Greece",
@@ -13,4 +13,4 @@ const jsonStr = `{
     "phoneNumber": null
 }`
 
-console.log(JSON.parse(jsonStr));   // deserialization
+console.log(JSON.parse(jsonStr)) // deserialization 
